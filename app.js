51f569b816556d036b25172d8befab6bd459b934
jkelly90 <jlfkelly@gmail.com>
2020-02-08 14:11:28 +0100

    added function to toggle between celsius and farenheit

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ window.addEventListener('load', () => {
     let temperatureDescription = document.querySelector('.temperature-description');
     let temperatureDegree = document.querySelector('.temperature-degree');
     let locationTimezone = document.querySelector('.location-timezone');
+    let temperatureSection = document.querySelector('.temperature-section');
+    let temperatureSpan = document.querySelector('.temperature-section span');
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
@@ -19,14 +21,35 @@ window.addEventListener('load', () => {
                 })
                 .then(data => {
                     console.log(data);
-                    const { temperature, summary } = data.currently;
+                    const { temperature, summary, icon } = data.currently;
                     //Set DOM elements from the API
                     temperatureDegree.textContent = temperature;
                     temperatureDescription.textContent = summary;
                     locationTimezone.textContent = data.timezone;
+                    //formula for celsius
+                    let celsius = (temperature - 32) * (5 / 9);
+                    //Set Icon
+                    setIcons(icon, document.querySelector('.icon'))
+
+                    //change temperature to celsius/farenheit
+                    temperatureSection.addEventListener('click', () => {
+                        if (temperatureSpan.textContent === "F") {
+                            temperatureSpan.textContent = "C";
+                            temperatureDegree.textContent = Math.floor(celsius);
+                        } else {
+                            temperatureSpan.textContent = "F";
+                            temperatureDegree.textContent = temperature;
+                        }
+                    });
+
                 });
         });
+    }
 
-
+    function setIcons(icon, iconID) {
+        const skycons = new Skycons({ color: "white" });
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        return skycons.set(iconID, Skycons[currentIcon]);
     }
 });
