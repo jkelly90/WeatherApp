433e28e1007d6e84ae12a308d7bea105ed420e7c
jkelly90 <jlfkelly@gmail.com>
2020-02-08 13:48:26 +0100

    fetched api

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 window.addEventListener('load', () => {
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
+
+            const proxy = `http://cors-anywhere.herokuapp.com/`;
+            const api = `${proxy}https://api.darksky.net/forecast/1562e26e9fa82a49c35ae27e6f848f05/${lat},${long}`;
+
+            fetch(api)
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                    const { temperature, summary } = data.currently;
+                    //Set DOM elements from the API
+                    temperatureDegree.textContent = temperature;
+                    temperatureDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone;
+                });
         });
-    } else {
-        h1.textContent = "please enable geo location";
+
+
     }
 });
